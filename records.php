6c28052995d12e947dac7866950653f9a9940adf
davno_pora <63275287+astratow@users.noreply.github.com>
2020-09-07 05:57:46 +0100

    Add files via upload

---
+++ records.php
@@ -0,0 +1,69 @@
+<?php
+include 'connect-db.php';
+function renderForm($first= '', $last= '', $error = '', $id = '')
+{?>
+<html lang="en">
+  <head>
+    <title>
+      <?php if($id !='') {echo "Edit Record";}else{echo "New Record";} ?>
+    </title>
+    <meta charset="UTF-8"/>
+  </head>
+  <body>
+    <h1><?php if($id !='') {echo "Edit Record";}else{echo "New Record";} ?>
+</h1> 
+    <?php if($error != ''){
+      echo "<div style='padding:4px; border=1px solid red; color:red'>" . $error . "</div>";  
+}?>
+  <form action="" method="post">
+  <div>
+    <?php if($id !=''){?>
+      <input type="hidden" name="id" value="<?php echo $id; ?>" />
+      <p>ID: <?php echo $id; ?> </p>
+    <?php } ?>
+
+    <strong>First Name: *</strong> <input type="text" name="firstname" value="<?php echo $first; ?>"/>
+      <br>
+    <strong>Last Name: *</strong> <input type="text" name="lastname" value="<?php echo $last; ?>"/>
+    <br>
+    <p> * required</p>
+    <br>
+    <input type="submit" name="submit" value="Submit"/>
+  </body>
+</html>
+<?php
+}
+if(isset($_GET['id'])){
+  //editing existing record
+  renderForm(NULL, NULL, NULL, $_GET['id']);
+}else{
+  //create new record
+  if(isset($_POST['submit'])){
+    $firstname = htmlentities($_POST['firstname'], ENT_QUOTES);
+    $lastname = htmlentities($_POST['lastname'], ENT_QUOTES);
+    
+    if($firstname == ''||$lastname =='')
+    {
+      $error = 'ERROR: Please fill in all required fields!';
+      renderForm($firstname, $lastname, $error);
+    }else{
+      if($stmt=$mysqli->prepare("INSERT players (firstname, lastname) VALUES(?, ?)")){
+        $stmt->bind_param("ss", $firstname, $lastname);
+        $stmt->execute();
+        $stmt->close();
+      }else{
+        echo "ERROR: Could not prepare SQL statement.";
+      }
+      header("Location: view.php");
+    }
+
+  }else{
+    
+    renderForm();
+  }
+  
+
+
+}
+$mysqli->close();
+?>
