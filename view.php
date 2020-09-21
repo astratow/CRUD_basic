a73c26a5eee2bec9ff95fc44b6a0f072beaefe18
davno_pora <63275287+astratow@users.noreply.github.com>
2020-09-21 04:31:21 +0100

    Update view.php
    Fixed table view

--- view.php
+++ view.php
@@ -17,7 +17,7 @@
         if($result->num_rows > 0)
         {
           echo "<table border='1' cellpadding='10'>";
-          echo "<td><th>ID</th><th>First Name</th><th>Last Name</th><th></th></tr>";
+          echo "<th>ID</th><th>First Name</th><th>Last Name</th><th></th><th></th></tr>";
 
           while($row = $result->fetch_object())
           {
@@ -25,6 +25,7 @@
             echo "<td>" . $row->id . "</td>";
             echo "<td>" . $row->firstname . "</td>";
             echo "<td>" . $row->lastname . "</td>";
+			//echo "<td>" . $row->dob . "</td>";
             echo "<td><a href='records.php?id=" . $row->id . "'>Edit</a></td>";
             echo "<td><a href='delete.php?id=" . $row->id . "'>Delete</a></td>";
             echo "</tr>";
